750c6e8d5eb4471136f4518a9b01a09fcaec1fbb
Nova Siska Nurfadilah <12150124481@students.uin-suska.ac.id>
2023-05-15 10:04:11 +0700

    quranku

---
+++ src/components/FooterItem.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: "FooterItem",
+};
+</script>
+
+<template>
+  <footer class="text-white p-1">
+    <div class="d-flex justify-content-around">
+      <p>&longleftarrow; By Nova Siska Nurfadilah</p>
+      <p></p>
+      <p>&copy; Al-Qur'anku </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  font-family: Joyce;
+  background-color: #a18484;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  animation: hue-rotate 2s linear infinite alternate;
+}
+@keyframes hue-rotate {
+  to {
+    filter: hue-rotate(90deg);
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<template>
+  <div class="h text-center">
+    <h2>Selamat Datang Di My Qur'an</h2>
+  </div>
+</template>
+<style>
+.h {
+  font-family: Joyce;
+  text-size: 200px;
+  margin-top: 200px;
+  color: #936f6f;
+}
+</style>
